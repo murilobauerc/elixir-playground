cbcdb6023c20db5982cddd427ed01626e148badf
murilobauerc <murilobauerc@gmail.com>
2021-10-12 10:36:03 -0300

    finished data structures with lists,tuples,maps

--- basics/data-structures.exs
+++ basics/data-structures.exs
@@ -1,7 +1,10 @@
 # Lists, tuples, maps on Elixir
+# List syntax []
+# Tuples syntax {}
+# Maps syntax %{}
 
 
-# Lists
+# Lists []
 list = [3.14, :pie, "Apple"]
 IO.inspect list
 
@@ -26,9 +29,44 @@ IO.inspect(tl list) # :pie, "Apple"
 # better way to have head and tail
 [head | tail] = [3.14, :pie, "Apple"]
 
-IO.inspect head
+IO.inspect "This is the head #{head}"
 IO.inspect tail
 
 
-# Tuples
+# Tuples {}
 
+tuple = {"banana", "apple", "strawberry"}
+IO.inspect(tuple)
+
+# Keyword lists - associative collections
+# Keys are atoms and ordered. "foo", "hello".
+# Keys are not strict equal.
+
+keyword_list = [foo: "bar", hello: "world"]
+IO.inspect(keyword_list)
+
+
+# Maps %{}
+
+map = %{:foo => "bar", "hello" => :world}
+IO.inspect(map)
+
+# access first value on the map
+IO.inspect(map[:foo])
+
+IO.inspect(map["hello"])
+
+# Maps with only atom keys 
+
+only_atom_keys_map = %{fruit: "banana", second_fruit: "apple"}
+generic_map = %{:fruit => "banana", :second_fruit => "apple"}
+
+IO.inspect(only_atom_keys_map == generic_map) # true
+
+IO.inspect(only_atom_keys_map.fruit)
+
+# Add a new key into map: `Map.put/3`
+
+new_map = %{hello: "world"}
+
+IO.inspect(Map.put(new_map, :foo, "baz"))
