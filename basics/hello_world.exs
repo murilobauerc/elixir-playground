54f85063b4388673ce20a93b0dc985a603aba609
Murilo Bauer <murilobauerc@gmail.com>
2021-10-21 23:14:11 -0300

    adding more comments to pipe solution

--- basics/hello_world.exs
+++ basics/hello_world.exs
@@ -2,5 +2,6 @@ IO.puts "Hello World"
 
 
 # Alternative solution using pipe operator
+# Not a best practice to use only one pipe operator only once
 
-"Hello World using Pipe :)" |> IO.puts
+"Hello World using Pipe :)" |> IO.puts()
