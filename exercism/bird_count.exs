430194b8a51fa07e95053396e32a1ac80fdb3496
murilobauerc <murilo.bauer@sumup.com>
2021-12-09 12:19:32 -0300

    Adds a new exercise bird_count through exercism

--- exercism/bird_count.exs
+++ exercism/bird_count.exs
@@ -1,20 +1,42 @@
 defmodule BirdCount do
-  def today(list) do
-    # Please implement the today/1 function
+  # Alternative solution would be List.first(list)
+  def today([head | tail]) do
+    head
   end
 
-  def increment_day_count(list) do
-    # Please implement the increment_day_count/1 function
+  def today([]) do
+    nil
   end
 
-  def has_day_without_birds?(list) do
-    # Please implement the has_day_without_birds?/1 function
+  def increment_day_count([head | tail]) do
+    [head + 1 | tail]
   end
 
-  def total(list) do
-    # Please implement the total/1 function
+  def increment_day_count([]) do
+    [1]
   end
 
-  def busy_days(list) do
+  def has_day_without_birds?([]) do
+     false
   end
+
+  def has_day_without_birds?([head | tail]) do
+     head == 0 or has_day_without_birds?(tail)
+  end
+
+  def total([]) do
+    0
+  end
+
+  def total([head | tail]) do
+    head + total(tail)
+  end
+
+  def busy_days([head | tail]) when head > 4, do: 1 + busy_days(tail)
+  def busy_days([head | tail]), do: busy_days(tail)
+
+ def busy_days([]) do
+    0
+  end
+
 end
